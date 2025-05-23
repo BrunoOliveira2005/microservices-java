cf5de480062ded111f10df357574de5c34137887
Bruno <bruno.pf2005@gmail.com>
2025-05-23 15:01:09 -0300

    Projeto atualizado corretamente

--- greating-service/src/main/java/br/edu/atitus/greating_service/GreatingController.java
+++ greating-service/src/main/java/br/edu/atitus/greating_service/GreatingController.java
@@ -1,5 +1,7 @@
 package br.edu.atitus.greating_service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,24 +20,24 @@ public class GreatingController {
 //	@Value("${greating-service.default-name}")
 //	private String defaultName;
 
-    //@Autowired
-    private final GreatingConfig greatingConfig;
+	//@Autowired
+	private final GreatingConfig greatingConfig; 
 
-    // Injeção de dependência via método construtor
-    public GreatingController(GreatingConfig greatingConfig) {
-        super();
-        this.greatingConfig = greatingConfig;
-    }
+	// Injeção de dependência via método construtor
+	public GreatingController(GreatingConfig greatingConfig) {
+		super();
+		this.greatingConfig = greatingConfig;
+	}
 
-    @GetMapping({ "", "/", "/{namePath}" })
-    public ResponseEntity<String> getGreating(@PathVariable(required = false) String namePath,
-                                              @RequestParam(required = false) String name) {
-        if (name == null)
-            name = namePath != null ? namePath : greatingConfig.getDefaultName();
+	@GetMapping({ "", "/", "/{namePath}" })
+	public ResponseEntity<String> getGreating(@PathVariable(required = false) String namePath,
+			@RequestParam(required = false) String name) {
+		if (name == null)
+			name = namePath != null ? namePath : greatingConfig.getDefaultName();
 
-        String returnText = String.format("%s %s!", greatingConfig.getGreating(), name);
+		String returnText = String.format("%s %s!", greatingConfig.getGreating(), name);
 
-        return ResponseEntity.ok(returnText);
-    }
+		return ResponseEntity.ok(returnText);
+	}
 
 }
