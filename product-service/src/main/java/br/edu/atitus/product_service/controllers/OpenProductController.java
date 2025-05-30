29e9f99bd164b89f61985fdbeecf88b7139bf078
Bruno <bruno.pf2005@gmail.com>
2025-05-30 13:38:02 -0300

    feat:Circuit Breaker no Product-Service com Resilience4j

--- product-service/src/main/java/br/edu/atitus/product_service/controllers/OpenProductController.java
+++ product-service/src/main/java/br/edu/atitus/product_service/controllers/OpenProductController.java
@@ -7,34 +7,55 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.atitus.product_service.clients.CurrencyClient;
+import br.edu.atitus.product_service.clients.CurrencyResponse;
 import br.edu.atitus.product_service.entities.ProductEntity;
 import br.edu.atitus.product_service.repositories.ProductRepository;
 
 @RestController
 @RequestMapping("products")
 public class OpenProductController {
-	
-	private final ProductRepository repository;
 
-	public OpenProductController(ProductRepository repository) {
+	private final ProductRepository repository;
+	private final CurrencyClient currencyClient;
+
+	public OpenProductController(ProductRepository repository, CurrencyClient currencyClient) {
 		super();
 		this.repository = repository;
+		this.currencyClient = currencyClient;
 	}
-	
+
 	@Value("${server.port}")
 	private int serverPort;
-	
+
 	@GetMapping("/{idProduct}/{targetCurrency}")
 	public ResponseEntity<ProductEntity> getProduct(
 			@PathVariable Long idProduct,
 			@PathVariable String targetCurrency
- 			) throws Exception {
-		
+	) throws Exception {
+
 		ProductEntity product = repository.findById(idProduct)
 				.orElseThrow(() -> new Exception("Product not found"));
-		
+
 		product.setEnviroment("Product-service running on Port: " + serverPort);
-		product.setConvertedPrice(product.getPrice());	
+
+		if (targetCurrency.equals(product.getCurrency()))
+			product.setConvertedPrice(product.getPrice());
+		else {
+
+			CurrencyResponse currency = currencyClient.getCurrency(product.getPrice(), product.getCurrency(), targetCurrency);
+			if (currency != null) {
+				product.setConvertedPrice(currency.getConvertedValue());
+				product.setEnviroment(product.getEnviroment() + " - " + currency.getEnviroment());
+
+
+			}else{
+				product.setConvertedPrice(-1);
+				product.setEnviroment(product.getEnviroment() + " - Currency Unavailable");
+			}
+		}
+
+
 		return ResponseEntity.ok(product);
 	}
 
